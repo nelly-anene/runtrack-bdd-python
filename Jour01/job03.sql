3781530902edf28475e4023021a83aaac238252a
nelly.anene <nelly.anene@laplateforme.io>
2026-03-02 23:26:10 +0100

    Job3 & Job4 : Creation table etudiants et affichage des colonnes

---
+++ Jour01/job03.sql
@@ -0,0 +1,4 @@
+-- Job 3 : Creation de la table etudiants
+CREATE TABLE etudiants (id INT NOT NULL AUTO_INCREMENT, nom VARCHAR(255) NOT NULL, prenom VARCHAR(25) NOT NULL, age INT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY (id));
+
+SHOW TABLES;
---
+++ Jour01/job04.sql
@@ -0,0 +1,2 @@
+-- Job 4 : Afficher les colonnes de la table etudiants
+DESCRIBE etudiants;
