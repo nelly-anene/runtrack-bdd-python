44829cbd2b63483fcfe2aa266d515cceb321aca0
nelly.anene <nelly.anene@laplateforme.io>
2026-03-03 21:38:35 +0100

    Jour01: correction job14 et job20

---
+++ Jour01/job14.sql
@@ -0,0 +1,2 @@
+-- Job 14 : Etudiants entre 18 et 25 ans tries par age croissant
+SELECT * FROM etudiants WHERE age BETWEEN 18 AND 25 ORDER BY age ASC;
---
+++ Jour01/job20.sql
@@ -0,0 +1,2 @@
+-- Job 20 : Compter le nombre d'etudiants mineurs
+SELECT COUNT(*) FROM etudiants WHERE age < 18;
